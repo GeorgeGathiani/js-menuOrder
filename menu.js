d40f52df44b9d8044b9500099f2788b236767589
GeorgeGathiani <george.gathiani@gmail.com>
2016-04-25 18:29:56 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,29 @@
 // begin with a blank/new order
-var order = '';
+var chapati = ["None", "Brown","Cheeseburger","White", ];
+var pilau  = ["None", "Kavu","Nyama"];
+var samosa = ["None", "Vegetable", "Beef", "Chicken"];
+
 
 
 // build a function to ask questions
 
 function question() {
-  // your code goes here
+  var orderPrompt = "Choose an option. Please enter number: \n";
+ var answerPrompt = "notanumber";
 }
 
+for(i=0;i<foodArray.length;i++){
+   orderPrompt += i + " - " + foodArray[i] + "\n";
+ }
+ while(isNaN(answerPrompt)){
+   answerPrompt = prompt(orderPrompt);
+ }
+ 
 // call the QUESTION function with the order options
-question();
+question(chapati);
+question(pilau);
+question(samosa);
+
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
